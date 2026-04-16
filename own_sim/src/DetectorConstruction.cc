67b7bb16ad3b744273a7b9fc39d10963e8bfb4f2
Duc Anh Nguyen <147715553+ducanhnguyen05@users.noreply.github.com>
2026-04-16 10:20:21 +0300

    Remove degree conversion from detector spread calculationfix

--- own_sim/src/DetectorConstruction.cc
+++ own_sim/src/DetectorConstruction.cc
@@ -128,7 +128,7 @@ void DetectorConstruction::SetDetectorStepDeg(G4double val)
 G4double DetectorConstruction::GetDetectorCapRadius() const
 {
   const G4double D = fConfig->detectorDistance;
-  const G4double spreadDeg = std::max(0.0, fConfig->detectorSpreadDeg)*deg;
+  const G4double spreadDeg = std::max(0.0, fConfig->detectorSpreadDeg);
   G4double rest_angle=(180-(90+spreadDeg))*deg ;
   
   G4double radius=std::sin(spreadDeg)*D/std::sin(rest_angle);
